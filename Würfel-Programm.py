f9f976cb0e8de124e71a8e6b6488c19c859c54be
Armin Alavi <arminalavi7879@gmail.com>
2024-10-28 18:31:02 +0100

    Update Würfel-Programm.py

--- Würfel-Programm.py
+++ Würfel-Programm.py
@@ -1,21 +1,15 @@
 import random
 
 def wuerfel_zaehlen(anzahl_wuerfel):
-    zaehler = [0] * 6  # Liste zur Zählung der Augenzahlen 1-6
-    
-    # Würfeln und Ergebnisse zählen
+    zaehler = [0] * 6
     i = 0
     while i < anzahl_wuerfel:
         wurf = random.randint(1, 6)
         zaehler[wurf - 1] += 1
         i += 1
-    
-    # Ergebnisse ausgeben
     zahl = 0
     while zahl < 6:
         print(f"{zahl + 1}: {zaehler[zahl]} mal")
         zahl += 1
-
-# Beispiel: Anzahl der Würfel eingeben
 anzahl = int(input("Giben Sie die Anzahl der Würfel ein: "))
 wuerfel_zaehlen(anzahl)
